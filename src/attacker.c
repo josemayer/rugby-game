977baf5cda9b282a89a25b66d2d4eba4a3ef9a77
José Lucas Silva Mayer <josemayer@usp.br>
2022-03-24 10:39:39 -0300

    Add attacker right-biased movement strategy
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- src/attacker.c
+++ src/attacker.c
@@ -13,17 +13,80 @@
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
 
 /*----------------------------------------------------------------------------*/
+/*                              PRIVATE FUNCTIONS                             */
+/*----------------------------------------------------------------------------*/
+
+void usualStateDirectionsArray(direction_t array[], int state);
+void swapPriorityUpDirection(direction_t array[]);
+void swapPriorityDownDirection(direction_t array[]);
+
+void usualStateDirectionsArray(direction_t array[], int state) {
+	if (state != 1) {
+		array[0] = (direction_t) DIR_RIGHT;
+		array[1] = (direction_t) DIR_UP_RIGHT;
+		array[2] = (direction_t) DIR_DOWN_RIGHT;
+		array[3] = (direction_t) DIR_UP;
+		array[4] = (direction_t) DIR_DOWN;
+		array[5] = (direction_t) DIR_UP_LEFT;
+		array[6] = (direction_t) DIR_DOWN_LEFT;
+		array[7] = (direction_t) DIR_LEFT;
+	}
+}
+
+void turnPriorityUpDirection(direction_t array[]) {
+	array[0] = (direction_t) DIR_UP_RIGHT;
+	array[1] = (direction_t) DIR_RIGHT;
+	array[2] = (direction_t) DIR_DOWN_RIGHT;
+}
+
+void turnPriorityDownDirection(direction_t array[]) {
+	array[0] = (direction_t) DIR_DOWN_RIGHT;
+	array[1] = (direction_t) DIR_RIGHT;
+	array[2] = (direction_t) DIR_UP_RIGHT;
+}
+
+/*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
 direction_t execute_attacker_strategy(
     position_t attacker_position, Spy defender_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(attacker_position);
-  UNUSED(defender_spy);
+	static int round = 0;
+	static int state = 0;
+	static position_t lastPosition = INVALID_POSITION;
+	static direction_t dirVector[8];
+	static int directionPointer = 0;
 
-  // TODO: Implement Attacker logic here
-  return (direction_t) DIR_RIGHT;
+	usualStateDirectionsArray(dirVector, state);
+	round++;
+	// A partir do 4º round é possível ter uma "visão geral" do movimento
+	// do defensor por meio de sua posição. Até lá, o atacante recua ou
+	// permanece na mesma região.
+	if (round < 4) 
+		return dirVector[7];
+	// No round 3, o spy é disparado e ocorre a verificação de se o defensor
+	// está acima ou abaixo da posição do atacante. Então, altera-se o vetor
+	// de direções para uma direção que seja oposta àquela da posição do
+	// defensor no momento do spy.
+	if (round == 4) {
+		position_t defenderPosition = get_spy_position(defender_spy);
+		state = 1;
+		if (defenderPosition.i - attacker_position.i > 0) {
+			turnPriorityUpDirection(dirVector);
+		} else {
+			turnPriorityDownDirection(dirVector);
+		}
+
+	}
+	
+	if (!equal_positions(lastPosition, attacker_position)) {
+		directionPointer = 0;
+	} else {
+		directionPointer++;
+	}
+
+	lastPosition = attacker_position;
+	return dirVector[directionPointer];
 }
 
 /*----------------------------------------------------------------------------*/
